16bf0b91b128668a500aa78bf5d0f39085967b2e
shubhamshewale96 <shubhamshewale1996@gmail.com>
2021-12-16 14:35:11 +0530

    solve error

--- src/main/java/com/h2database/H2databaseApplication.java
+++ src/main/java/com/h2database/H2databaseApplication.java
@@ -2,12 +2,15 @@ package com.h2database;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class H2databaseApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(H2databaseApplication.class, args);
+
+        ApplicationContext context = SpringApplication.run(H2databaseApplication.class, args);
+        System.out.println(context.getBean(H2databaseApplication.class));
     }
 
 }
